73818783ef26e43412e2e3366396bf41a5651329
ambition-kwon <prince5390@gmail.com>
2024-07-02 17:19:06 +0900

    [add] pybo index.html, detail.html

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import Question, Answer
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
@@ -10,3 +10,12 @@ def index(request):
     }
     # render로 넘길 때는 무조건 딕셔너리 객체여야함
     return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        "question_subject": question.subject,
+        "question_content": question.content,
+        "question_created_at": question.created_at,
+    }
+    return render(request, "pybo/question_detail.html", context)
