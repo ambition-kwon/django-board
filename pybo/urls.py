1bd1b3bc70c36d6d06cbac3d753b5dfc70640671
ambition-kwon <prince5390@gmail.com>
2024-07-02 17:52:23 +0900

    [add] create page

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+
 from . import views
 
+#alias 중복 방지를 위한 별칭 지정
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index),
-    path('<int:question_id>/', views.detail)
+    # name값은 별칭(alias) 지정. 즉, pybo/는 index라는 이름, pybo/int/는 detail이라는 이름.
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
-from .models import Question, Answer
 from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 
 def index(request):
@@ -8,7 +8,7 @@ def index(request):
     context = {
         'question_list': question_list
     }
-    # render로 넘길 때는 무조건 딕셔너리 객체여야함
+    # render 로 넘길 때는 무조건 dictionary 객체 여야함
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
@@ -17,5 +17,10 @@ def detail(request, question_id):
         "question_subject": question.subject,
         "question_content": question.content,
         "question_created_at": question.created_at,
+        "question_id": question_id,
     }
     return render(request, "pybo/question_detail.html", context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answers.create(content=request.POST['content'])
